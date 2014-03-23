233e447fa9b3a0abef618c3e4fcc3b1ab04440f7
Thane Vo <vothane@gmail.com>
2014-03-23 00:33:08 -0600

    add test for generating correct http request hash signatures

---
+++ test/voodoo/ooyala-test.clj
@@ -0,0 +1,14 @@
+(ns voodoo.ooyala-test
+  (:require [clojure.test  :refer :all]
+            [voodoo.ooyala :refer :all]))
+
+(def ^{:dynamic true} *api-key*    "7ab06")
+(def ^{:dynamic true} *secret-key* "329b5b204d0f11e0a2d060334bfffe90ab18xqh5")
+
+(deftest request-test-one
+  (let [http-method         :post
+        request-path        "/v2/players/HbxJKM"
+        query-string-params {:api_key *api_key* :expires 1299991855}
+        request-body        "test"]
+    (is (= "fJrWCcIqeRBZUqa61OV%2B6XOWfpkab6RdW5hJZmZh1CI" 
+           (request-signature secret-key http-method request-path query-string-params request-body)))))
