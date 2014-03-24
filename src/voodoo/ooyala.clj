d6efa4c39fa69271e2b22f775d4f822e1e8f64cc
Thane Vo <vothane@gmail.com>
2014-03-23 21:56:46 -0600

    fix Base64 hashing

--- src/voodoo/ooyala.clj
+++ src/voodoo/ooyala.clj
@@ -4,12 +4,12 @@
 
 (def ^{:dynamic true} *hash* "SHA-256")
 
+
 (defn request-signature [secret http-method request-path query-string-params request-body]
-  (let [seed-string      (str secret http-method "/v2/" request-path )
+  (let [seed-string      (str secret http-method "/v2/" request-path)
         sorted-params    (sort query-string-params)
-        signature-string (str (reduce (fn [s k=v] (str s k=v)) seed-string
+        signature-string (str (reduce (fn [seed+= k=v] (str seed+= k=v)) seed-string
                                 (map (fn [param] (str (first param) "=" (second param))) sorted-params))
                            request-body)
-        hash-signature   (.encodeBase64String (Base64.)
-                           (.digest (MessageDigest/getInstance *hash*) (.getBytes signature-string)))]
+        hash-signature   (Base64/encodeBase64String (.digest (MessageDigest/getInstance *hash*) (.getBytes signature-string)))]
     (subs hash-signature 0 43)))
