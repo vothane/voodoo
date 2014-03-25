2804874e5f8818b8380bd1bc05edc414040881d2
Thane Vo <vothane@gmail.com>
2014-03-24 22:00:46 -0600

    fix Base64 hashing

--- src/voodoo/ooyala.clj
+++ src/voodoo/ooyala.clj
@@ -1,9 +1,8 @@
 (ns voodoo.ooyala
-  (:import [java.security MessageDigest]
-           [org.apache.commons.codec.binary Base64]))
+  (:require [clojure.data.codec.base64 :as Base64])
+  (:import [java.security MessageDigest]))
 
 (def ^{:dynamic true} *hash* "SHA-256")
-
 
 (defn request-signature [secret http-method request-path query-string-params request-body]
   (let [seed-string      (str secret http-method "/v2/" request-path)
@@ -11,5 +10,5 @@
         signature-string (str (reduce (fn [seed+= k=v] (str seed+= k=v)) seed-string
                                 (map (fn [param] (str (first param) "=" (second param))) sorted-params))
                            request-body)
-        hash-signature   (Base64/encodeBase64String (.digest (MessageDigest/getInstance *hash*) (.getBytes signature-string)))]
-    (subs hash-signature 0 43)))
+        hash-signature   (Base64/encode (.digest (MessageDigest/getInstance *hash*) (.getBytes signature-string)))]
+    (subs (String. hash-signature) 0 43)))
