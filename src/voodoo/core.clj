eff99a9e89dd07feb72cee3a2b047d61d245a439
Thane Vo <vothane@gmail.com>
2014-03-26 23:11:30 -0600

    fix test and implementation for http-request

--- src/voodoo/core.clj
+++ src/voodoo/core.clj
@@ -6,6 +6,10 @@
   [verb uri arg-map]
   (let [body      (:body arg-map)
         query     (reduce (fn [seed+= k=v] (str seed+= k=v)) "?"
-                     (map (fn [param] (str (name (first param)) "=" (second param))) (:query (:params arg-map))))
-        final-uri (str uri query)]
-   (final-uri)))
+                     (map (fn [param] (str (name (first param)) "=" (second param))) (:query (:params arg-map))))]
+   (str uri query)))
+
+(defn http-request
+  [verb uri parameters]
+  (let [request-args (get-request-args verb uri parameters)]
+    (str request-args)))
--- test/voodoo/core_test.clj
+++ test/voodoo/core_test.clj
@@ -2,9 +2,9 @@
   (:require [clojure.test :refer :all]
             [voodoo.core :refer :all]))
 
-(deftest get-request-args
+(deftest http-request-test
   (let [verb   "get"
         uri    "api.ooyala.com/v2/assets"
         params {:params {:query {:where "labels INCLUDES 'Sports' AND labels INCLUDES 'Baseball'"}}}
         actual "api.ooyala.com/v2/assets?where=labels INCLUDES 'Sports' AND labels INCLUDES 'Baseball'"]
-    (is (= (get-request-args verb uri params) actual))))
+    (is (= (http-request verb uri params) actual))))
