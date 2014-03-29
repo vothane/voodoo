aba27b3a3b89cc2ce0f2bbf537a98662d50fda58
Thane Vo <vothane@gmail.com>
2014-03-28 23:59:34 -0600

    get url correct query string

--- src/voodoo/query.clj
+++ src/voodoo/query.clj
@@ -1,7 +1,8 @@
 (ns voodoo.query
   (:require [clojure.string :as str]))
 
-(defn expand-expr [expr]
+(defn expand-expr 
+  [expr]
   (if (coll? expr)
     (if (= (first expr) `unquote)
       "?"
@@ -14,12 +15,15 @@
 (def clause-map
   {'WHERE (fn [expr] (str "where=" (expand-expr expr)))})
 
-(defn expand-clause [[op & args]]
+(defn expand-clause 
+  [[op & args]]
   (apply (clause-map op) args))
 
-(defmacro WHERE [& args]
+(defmacro WHERE 
+  [& args]
   [(expand-clause (cons 'WHERE args))
    (vec (for [n (tree-seq coll? seq args) :when (and (coll? n) (= (first n) `unquote))] (second n)))])
 
-;(reduce (fn [a b] (str/replace-first a "?" b)) (first t) (second t))
-;"where=duration < 600 AND description = cat funny"
+(defn get-ooyala-query
+  [query-with-clauses]
+  (reduce (fn [query-string clause] (str/replace-first query-string "?" clause)) (first query-with-clauses) (second query-with-clauses)))
--- test/voodoo/query_test.clj
+++ test/voodoo/query_test.clj
@@ -1,13 +1,14 @@
 (ns voodoo.query-test
-  (:require [clojure.test  :refer :all]
+  (:require [clojure.test :refer :all]
             [voodoo.query :refer :all]))
 
 (defn query [max desc]
   (WHERE (AND (< duration ~max) (= description ~desc))))
 
 (deftest query-test
-  (let [result (query 600 "cat funny")
-        actual ["where=duration < ? AND description = ?" [600 "cat funny"]]]
+  (let [result (get-ooyala-query (query 600 "cat funny"))
+        _      (println result)
+        actual "where=duration < 600 AND description = cat funny"]
     (is (= result actual))))
 
 ;(reduce (fn [a b] (str/replace-first a "?" b)) (first t) (second t))
