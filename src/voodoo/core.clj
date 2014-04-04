03b8c999a8b2ff314b5f87ff0d32b7be7f7d5b05
Thane Vo <vothane@gmail.com>
2014-04-03 22:14:50 -0600

    refactor method creating macros

--- src/voodoo/core.clj
+++ src/voodoo/core.clj
@@ -14,22 +14,21 @@
    (str uri "?" query)))
 
 (def http-request-map
-  {"GET"  (fn [uri] (client/get (str "http://" uri)))
+  {"GET"  (fn [uri & ignore] (client/get uri))
    "POST" (fn [uri body] (client/post uri body))})
 
 (defn http-request
-  [verb uri parameters]  
+  [verb uri parameters body]  
   (let [sorted-params (into (sorted-map) parameters)  
         get-request   (http-request-map verb)        
         request-uri   (get-request-uri uri sorted-params)] 
-    (try            
-      (get-request request-uri)
-      (catch Exception e (str "caught exception: " (.getMessage e))))))
+    (try                
+      (get-request request-uri body)
+    (catch Exception e (str "caught exception: " (.getMessage e))))))
 
 (defmacro def-ooyala-method
-  [fn-name verb resource-path]
-  `(defn ~fn-name
-     [params#]
+  [verb resource-path]
+  `(fn [& [params#]]
      (let [http-verb#  (s/upper-case (name ~verb))
            api-key#    {:api_key *api-key*}
            expiration# {:expires (oo/expires)}
@@ -38,12 +37,5 @@
            params-map# (merge api-key# expiration# query#)
            signature#  {:signature (oo/request-signature *secret* http-verb# ~(str "/v2/" resource-path) params-map# body#)}
            all-params# (merge params-map# signature#)
-           uri#        (str *rest-api* "/" ~resource-path)]
-       (http-request http-verb# uri# all-params#))))
-
-(defmacro def-ooyala-restful-method
-  [verb resource-path & options]
-  (let [dashed-name (clojure.string/replace resource-path #"[^0-9a-zA-Z]+" "-")
-        clean-name  (clojure.string/replace dashed-name #"-$" "")
-        fn-name     (symbol clean-name)]
-    `(def-ooyala-method ~fn-name ~verb ~resource-path ~@options)))
+           uri#        (str "https://" *rest-api* "/" ~resource-path)]
+       (http-request http-verb# uri# all-params# body#))))
--- test/voodoo/core_test.clj
+++ test/voodoo/core_test.clj
@@ -5,7 +5,7 @@
             [voodoo.ooyala :refer :all]
             [clj-http.client :as client]))
 
-(def-ooyala-restful-method :get "assets")
+(def find-video (def-ooyala-method :get "assets"))
 
 (defn query [max desc]
   (WHERE (AND (> duration ~max) (= description ~desc))))
@@ -14,7 +14,7 @@
 
   (testing "search vid with duration > 10 secs and has 'Iron Man, Thor, Captain America, and the Hulk' in description"
     (let [query-string (get-ooyala-query (query 10 "Iron Man, Thor, Captain America, and the Hulk"))
-          response     (assets {:params {:query {:where query-string}}})
+          response     (find-video {:params {:query {:where query-string}}})
           result       (get-response-data response)]
       (is (= (:status response) 200))   
       (is (= (get result "name") "Avengers"))
@@ -26,36 +26,36 @@
       (is (= (get result "embed_code") "o1NmdxMzrrWwbOVk_wIqhw-AmhlOMO49")))))
 
 
-(def-ooyala-restful-method :get "labels")
+(def get-labels (def-ooyala-method :get "labels"))
 
 (deftest labels-test
 
   (testing "get all labels"
-    (let [response (labels {})
+    (let [response (get-labels)
           result   (get-response-data response)]
       (is (= (:status response) 200))
       (is (= (get result "full_name") "/Movie Trailer"))
       (is (= (get result "id") "b5e9bf9a0e5a4b31991cfdf9218c4342")))))
 
 
-(def-ooyala-restful-method :get "players")
+(def get-players (def-ooyala-method :get "players"))
 
 (deftest players-test
 
   (testing "get meta-data for all video players"
-    (let [response (players {})
+    (let [response (get-players)
           result   (get-response-data response)]
       (is (= (:status response) 200))
       (is (= (get result "name") "test player"))
       (is (= (get result "playback") {"buffer_on_pause" false})))))
 
 
-(def-ooyala-restful-method :get "players/447b53e1c17c447f8dbbd912b1fbc522")
+(def get-player (def-ooyala-method :get "players/447b53e1c17c447f8dbbd912b1fbc522"))
 
 (deftest find-player-by-id-test
 
   (testing "get meta-data for particular player with id of 447b53e1c17c447f8dbbd912b1fbc522"
-    (let [response (players-447b53e1c17c447f8dbbd912b1fbc522 {})
+    (let [response (get-player)
           result   (get-response-data response)
           _ (println result)]
       (is (= (:status response) 200))
